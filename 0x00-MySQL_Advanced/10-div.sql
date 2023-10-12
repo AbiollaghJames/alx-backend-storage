d8321857c2b705a6df4856a29ac806bf07c748a8
AbiollaghJames <abiollajames@protonmail.com>
2023-10-12 14:36:11 +0000

    Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -6,11 +6,7 @@ DELIMITER //
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 BEGIN
-	IF b > 0
-		RETURN a / b;
-	ELSE
-		RETURN 0;
-	END IF;
+	RETURN (IF (b = 0, 0, a / b));
 END //
 
 DELIMITER ;
