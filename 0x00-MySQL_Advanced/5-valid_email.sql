f834ecdc37e03b1fa3e7c4d1cd1060e2df11da76
AbiollaghJames <abiollajames@protonmail.com>
2023-10-12 07:59:04 +0000

    Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -2,11 +2,14 @@
 -- valid_email only when email is changed.
 -- use TRIGGER
 
+DELIMITER$$
+
 CREATE TRIGGER reset_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-BEGIN
-	IF OLD.email != NEW.email THEN
-		SET NEW.valid_email = 0;
-	END IF;
-END;
+	BEGIN
+		IF OLD.email != NEW.email THEN
+			SET NEW.valid_email = 0;
+		END IF;
+END$$
+DELIMITER;
