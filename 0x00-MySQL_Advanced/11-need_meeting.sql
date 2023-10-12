2347c285b1fa9c2eb14044ce147de9519b3cd8fb
AbiollaghJames <abiollajames@protonmail.com>
2023-10-12 14:45:46 +0000

    No table for a meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- create a view that lists all students with score under 80
+-- and no last meetiing or more than 1 month
+
+CREATE VIEW need_meeting AS
+SELECT name
+FROM students
+WHERE score < 80
+AND
+(students.last_meeting IS NULL OR students.last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
