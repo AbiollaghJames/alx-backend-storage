a6a866c7662059ac96e0b92577636576135c6f57
AbiollaghJames <abiollajames@protonmail.com>
2023-10-12 06:33:22 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- ranked by their longevity
 -- use of COALESE
 
-SELECT band_name, COALESE(split, 2022) - formed as lifespan FROM metal_bands
+SELECT band_name, COALESCE(split, 2022) - formed as lifespan FROM metal_bands
 WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
