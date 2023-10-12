06dcf98403278b98394e777e4c8698e0a2a13104
AbiollaghJames <abiollajames@protonmail.com>
2023-10-12 13:49:01 +0000

    Optimize simple search

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,4 +1,4 @@
 -- script that creates an index on table names and the
 -- first letter of name
 
-CREATE INDEX idx_name_first ON names(LEFT(name, 1));
+CREATE INDEX idx_name_first ON names(name(1));
