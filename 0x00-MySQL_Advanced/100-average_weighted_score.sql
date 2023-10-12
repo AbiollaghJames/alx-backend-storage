76e0d086a0d56fc97662003ac805f9e4a4287f76
AbiollaghJames <abiollajames@protonmail.com>
2023-10-12 15:07:51 +0000

    Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -6,12 +6,11 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightScoreForUser(IN user_id INT)
 BEGIN
 	UPDATE users
-	SET average_weighted_score = (
-	SELECT SUM(corrections.score * projects.weight) / NULLIF(SUM(projects.weight))
+	SET average_score = (
+	SELECT COALESCE(SUM(corrections.score * projects.weight) / NULLIF(SUM(projects.weight), 0), NULL)
 	FROM corrections
 	INNER JOIN projects
 	ON projects.id = corrections.project_id
-
 	WHERE corrections.user_id = user_id)
 	WHERE users.id = user_id;
 END //
