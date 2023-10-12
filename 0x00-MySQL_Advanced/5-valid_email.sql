0c75eb7c54d4f574fdc4a99406b3d7b8ab0ab348
AbiollaghJames <abiollajames@protonmail.com>
2023-10-12 07:43:53 +0000

    Email validation to sent

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- script that creates trigger that resets attribute
+-- valid_email only when email is changed.
+-- use TRIGGER
+
+CREATE TRIGGER reset_email
+AFTER UPDATE ON users
+FOR EACH ROW
+BEGIN
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 1;
+	END IF;
+END;
